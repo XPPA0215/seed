ea964331b8c2af90f13eaa2de7589d484bd63db4
Sam <samyan@xiaojing0.com>
2021-01-11 01:37:32 +0800

    feat.router

--- routers/router.go
+++ routers/router.go
@@ -3,12 +3,17 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	//"leopo/global"
-	"leopo/routers/users"
+	"leopo/routers/user"
 )
 
 func InitWebRouter() *gin.Engine {
 	var router = gin.Default()
 	Api := router.Group("/api")
-	Api.GET("/users", users.A)
+	{
+		userGroup := Api.Group("/user")
+		{
+			userGroup.GET("/a", user.A)
+		}
+	}
 	return router
 }
